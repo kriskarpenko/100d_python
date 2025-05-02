7fad846eeec733d90f0a4e1f3cd52ee3cc6e6bb0
Kristina Karpenko <karpenko.krista@gmail.com>
2025-05-02 01:39:37 +0100

    update: day 17

--- Intermediate/Day-17/quiz-game-start/main.py
+++ Intermediate/Day-17/quiz-game-start/main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for i in question_data:
@@ -8,5 +9,11 @@ for i in question_data:
     new_question = Question(text, answer)
     question_bank.append(new_question)
 
+quiz_brain = QuizBrain(question_bank)
 
-        
+while quiz_brain.still_has_questions():
+    quiz_brain.next_question()
+
+print("Yuppie! You have completed the quiz")
+print(f"You're final score is {quiz_brain.u_score}/{len(question_bank)}")
+
--- Intermediate/Day-17/quiz-game-start/quiz_brain.py
+++ Intermediate/Day-17/quiz-game-start/quiz_brain.py
@@ -2,6 +2,26 @@ class QuizBrain:
     def __init__(self, question_list):
         self.question_index = 0
         self.question_list = question_list
+        self.u_score = 0
     
-    # def next_question():
+    def still_has_questions(self):
+        return self.question_index < len(self.question_list) #will return a boolean
         
+    def check_answer(self, u_answer, correct_answer):
+        self.u_answer = u_answer
+        self.correct_answer = correct_answer
+        if u_answer == correct_answer:
+            self.u_score += 1
+            print(f"Yay")
+        else:
+            print("Nay")
+        print(f"You're score is {self.u_score}/{self.question_index}")
+        print("\n")
+
+
+
+    def next_question(self):
+        current_question =  self.question_list[self.question_index]
+        self.question_index += 1
+        u_answer = input(f"Q.{self.question_index} {current_question.text} True/False?").capitalize()
+        self.check_answer(u_answer, current_question.answer)
