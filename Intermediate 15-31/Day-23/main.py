da06ce92ea4839f18c8ddee6931ec89d0f6815f7
Kristina Karpenko <karpenko.krista@gmail.com>
2025-05-19 23:54:26 +0100

    Day 23

--- Intermediate 15-31/Day-23/main.py
+++ Intermediate 15-31/Day-23/main.py
@@ -9,14 +9,31 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(player.move, "Up")
+screen.onkey(player.go_up, "Up")
 
-is_on = True
-while is_on:
-    speed = 0.1
-    time.sleep(speed)
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
     screen.update()
 
+    car_manager.create_car()
+    car_manager.move()
 
+    #Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    #Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+
+screen.exitonclick()
