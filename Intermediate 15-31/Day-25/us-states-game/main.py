6dad72bdc21eeb6d8091d0d66367ff26caca65c5
Kristina Karpenko <karpenko.krista@gmail.com>
2025-09-10 12:10:17 +0100

    Day 25 start

---
+++ Intermediate 15-31/Day-25/us-states-game/main.py
@@ -0,0 +1,13 @@
+import turtle
+
+img_url = 'blank_states_img.gif'
+
+screen = turtle.Screen()
+
+screen.title("U.S Name the State Game")
+screen.addshape(img_url)
+turtle.shape(img_url)
+
+
+
+screen.exitonclick()
